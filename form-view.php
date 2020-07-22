ec955cdf44e25d4c13a3e2a4371c51f934aee974
yoann stas <yoann.stas@me.com>
2020-07-22 16:50:04 +0200

    fixed broken link

--- form-view.php
+++ form-view.php
@@ -166,15 +166,18 @@ function testInput($data)
 
         <fieldset>
             <legend>Products</legend>
+            <?php foreach ($products AS $i => $product): ?>
                 <label>
                     <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
                     &euro; <?php echo number_format($product['price'], 2) ?></label><br />
             <?php endforeach; ?>
         </fieldset>
+
         <label>
             <input type="checkbox" name="express_delivery" value="5" />
             Express delivery (+ 5 EUR)
         </label>
+
         <button type="submit" class="btn btn-primary">Order!</button>
     </form>
 
